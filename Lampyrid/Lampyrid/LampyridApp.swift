3c0703cd0fc8766ee680b77228303114778783c6
Alex <alex.thgil@gmail.com>
2024-10-08 17:15:15 +0300

    initial state

---
+++ Lampyrid/Lampyrid/LampyridApp.swift
@@ -0,0 +1,17 @@
+//
+//  LampyridApp.swift
+//  Lampyrid
+//
+//  Created by Alex on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct LampyridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
